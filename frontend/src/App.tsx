9e7516aa638e5a33ed83216ed2b786498d842165
suryausman313-cell <suryausman313@gmail.com>
2026-08-15 21:30:32 +0400

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,16 @@ import SuperAdmin from './pages/SuperAdmin';
 import ShopAdmin from './pages/ShopAdmin';
 import Kitchen from './pages/Kitchen';
 import Rider from './pages/Rider';
+import CustomerAccount from './pages/CustomerAccount';
+import CustomerOrders from './pages/CustomerOrders';
 
 export default function App(){
  return <Routes>
   <Route path="/" element={<Marketplace/>}/>
   <Route path="/shop/:slug" element={<ShopPage/>}/>
   <Route path="/track/:orderId" element={<TrackOrder/>}/>
+  <Route path="/account" element={<CustomerAccount/>}/>
+  <Route path="/orders" element={<CustomerOrders/>}/>
   <Route path="/super-admin" element={<SuperAdmin/>}/>
   <Route path="/shop-admin" element={<ShopAdmin/>}/>
   <Route path="/kitchen" element={<Kitchen/>}/>
