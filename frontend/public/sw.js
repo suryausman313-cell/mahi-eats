b17a90f9311bd36962736d93eb79ab5978cba8da
suryausman313-cell <suryausman313@gmail.com>
2026-08-16 00:26:32 +0400

    Add files via upload

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mahi-eats-v2';
+const CACHE='mahi-eats-v6';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/mahi-icon.svg']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).pathname.startsWith('/api/'))return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))))});
